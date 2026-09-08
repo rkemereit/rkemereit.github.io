6b14c089c55fbe4a85beb68ec2cdbc02a53a1183
rkemereit <kemereitrichard@gmail.com>
2026-09-08 10:49:27 -0500

    Create asn2.js

---
+++ web200/asn2/asn2.js
@@ -0,0 +1,11 @@
+const student_name = "Scooby Doo";
+
+var age = 0;
+
+age = 176;
+
+let isalive = false;
+
+document.getElementById("studentname").innerHTML = "Student Name: " + student_name;
+document.getElementById("Age").innerHTML = "Age: " + age;
+document.getElementById("isalive").innerHTML = "Is Alive: " + isalive;
